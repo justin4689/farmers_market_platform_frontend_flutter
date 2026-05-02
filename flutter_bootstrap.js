9d86033295d90d318c47f4e7bf49701d568671da
justin <justintrah8@gmail.com>
2026-05-02 18:39:07 +0000

    Update application icon

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4184761320"
+    serviceWorkerVersion: "2430495178"
   }
 });
